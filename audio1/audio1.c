975365fdbde6d6d6f217264af8515c74eb229d2e
James Coxon <jacoxon@googlemail.com>
2013-09-19 11:25:36 +0100

    added in basic graph function

--- audio1/audio1.c
+++ audio1/audio1.c
@@ -163,9 +163,101 @@ int read_adc(void){
 	return reg16;
 }
 
+void generate_graph(int raw_data[256]){
+
+    //First define our 10 blocks, each will be a printed string of 20 chars long
+    //0
+    char line0[20] = "                   ";
+    //1
+    char line1[20] = "                   ";
+    //2
+    char line2[20] = "                   ";
+    //3
+    char line3[20] = "                   ";
+    //4
+    char line4[20] = "                   ";
+    //5
+    char line5[20] = "                   ";
+    //6
+    char line6[20] = "                   ";
+    //7
+    char line7[20] = "                   ";
+    //8
+    char line8[20] = "                   ";
+    //9
+    char line9[20] = "                   ";
+    
+    //Now we work through our sample and look at the value and put the corresponding point in the right place.
+    
+    int gap_between_samples = 256 / 20;
+    uint8_t i;
+    uint8_t pos = 0;
+    
+    send_string("Start graph");
+    send_newline();
+    for(i=0; i < 20; i++){
+        send_string("o");
+        
+        if(raw_data[i] < 410) {line0[pos] = '*';}
+        else if((raw_data[i] >= 410) & (raw_data[i] < 820)){line1[pos] = '*';}
+        else if((raw_data[i] >= 820) & (raw_data[i] < 1230)){line2[pos] = '*';}
+        else if((raw_data[i] >= 1230) & (raw_data[i] < 1640)){line3[pos] = '*';}
+        else if((raw_data[i] >= 1640) & (raw_data[i] < 2050)){line4[pos] = '*';}
+        else if((raw_data[i] >= 2050) & (raw_data[i] < 2460)){line5[pos] = '*';}
+        else if((raw_data[i] >= 2460) & (raw_data[i] < 2870)){line6[pos] = '*';}
+        else if((raw_data[i] >= 2870) & (raw_data[i] < 3280)){line7[pos] = '*';}
+        else if((raw_data[i] >= 3280) & (raw_data[i] < 3690)){line8[pos] = '*';}
+        else if(raw_data[i] >= 3690){line9[pos] = '*';}
+        else {};
+         
+        /*
+         if(raw_data[i] < 0.2) {line0[pos] = '*';}
+         else if((raw_data[i] >= 0.2) & (raw_data[i] < 0.4)){line1[pos] = '*';}
+         else if((raw_data[i] >= 0.4) & (raw_data[i] < 0.6)){line2[pos] = '*';}
+         else if((raw_data[i] >= 0.6) & (raw_data[i] < 0.8)){line3[pos] = '*';}
+         else if((raw_data[i] >= 0.8) & (raw_data[i] < 1.0)){line4[pos] = '*';}
+         else if((raw_data[i] >= 1.0) & (raw_data[i] < 1.2)){line5[pos] = '*';}
+         else if((raw_data[i] >= 1.2) & (raw_data[i] < 1.4)){line6[pos] = '*';}
+         else if((raw_data[i] >= 1.4) & (raw_data[i] < 1.6)){line7[pos] = '*';}
+         else if((raw_data[i] >= 1.6) & (raw_data[i] < 1.8)){line8[pos] = '*';}
+         else if(raw_data[i] >= 1.8){line9[pos] = '*';}
+         else {};
+         */
+        pos++;
+    }
+    send_newline();
+    send_string("Print Graph");
+    send_newline();
+    
+    //Now print the data
+    send_string(line0);
+    send_newline();
+    send_string(line1);
+    send_newline();
+    send_string(line2);
+    send_newline();
+    send_string(line3);
+    send_newline();
+    send_string(line4);
+    send_newline();
+    send_string(line5);
+    send_newline();
+    send_string(line6);
+    send_newline();
+    send_string(line7);
+    send_newline();
+    send_string(line8);
+    send_newline();
+    send_string(line9);
+    send_newline();
+    
+    
+}
+
 int main(void)
 {
     int analog_data = 0;
+    int testInput_f32_10khz[256];
     char superbuffer [80]; //Telem string buffer
     
 	clock_setup();
@@ -176,23 +268,28 @@ int main(void)
     gpio_set(GPIOD, GPIO12);
 
     
+    send_string("Starting data collection");
+    send_newline();
 	/* Blink the LED (PC8) on the board. */
 	while (1) {
         
-        //gpio_set(GPIOD, GPIO13);
-        
-        //send_string("TEST");
-        analog_data = read_adc();
-        //usart_send_blocking(USART2, analog_data);
-        //send_newline();
-        
-        sprintf (superbuffer, "%d", analog_data);
-        
-        send_string(superbuffer);
+        send_string("ADC");
+        send_newline();
+
+        uint8_t i;
+        for(i=0; i<200; i++){
+            testInput_f32_10khz[i] = read_adc();
+            send_string(".");
+        }
         send_newline();
         
-        //gpio_clear(GPIOD, GPIO13);
-        //delay_ms(100);
+        send_string("Collected data, graphing..."); send_newline();
+        generate_graph(testInput_f32_10khz);
+        
+        send_string("Completed");
+        send_newline();
+
+        delay_ms(10000);
         
     }
 
