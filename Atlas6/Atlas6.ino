4b2ed03a7401e2304098fc894b39af0cf75ffa8e
James Coxon <jacoxon@googlemail.com>
2013-12-28 00:31:41 +0000

    added in slow-hell every 20 strings, also added 2 further fields, voltage and internal temp. voltage doesn't seem to work and internal temp is difficult to intepret! Currently flight ready

--- Atlas6/Atlas6.ino
+++ Atlas6/Atlas6.ino
@@ -12,10 +12,122 @@ volatile char c;
 
 //Variables
 int32_t lat = 514981000, lon = -530000, alt = 0;
-uint8_t gps_hour = 0, gps_minute = 0, gps_second = 0, gps_month = 0, gps_day = 0, lock = 0, sats = 0, navmode = 0, n = 1, GPSerror = 0;
+uint8_t gps_hour = 0, gps_minute = 0, gps_second = 0, gps_month = 0, gps_day = 0, lock = 0, sats = 0, navmode = 0, n = 1, GPSerror = 0, voltage, int_temp;
 char superbuffer [80]; //Telem string buffer
 uint8_t buf[60]; //GPS receive buffer
 int count = 0;
+
+struct t_htab { char c; int hellpat[5]; } ;
+
+struct t_htab helltab[] = {
+
+  {'1', { B00000100, B00000100, B01111100, B00000000, B00000000 } },
+  {'2', { B01001000, B01100100, B01010100, B01001100, B01000000 } },
+  {'3', { B01000100, B01000100, B01010100, B01010100, B00111100 } },
+  {'4', { B00011100, B00010000, B00010000, B01111100, B00010000 } },
+  {'5', { B01000000, B01011100, B01010100, B01010100, B00110100 } },
+  {'6', { B00111100, B01010010, B01001010, B01001000, B00110000 } },
+  {'7', { B01000100, B00100100, B00010100, B00001100, B00000100 } },
+  {'8', { B00111100, B01001010, B01001010, B01001010, B00111100 } },
+  {'9', { B00001100, B01001010, B01001010, B00101010, B00111100 } },
+  {'0', { B00111000, B01100100, B01010100, B01001100, B00111000 } },
+  {'A', { B01111000, B00101100, B00100100, B00101100, B01111000 } },
+  {'B', { B01000100, B01111100, B01010100, B01010100, B00101000 } },
+  {'C', { B00111000, B01101100, B01000100, B01000100, B00101000 } },
+  {'D', { B01000100, B01111100, B01000100, B01000100, B00111000 } },
+  {'E', { B01111100, B01010100, B01010100, B01000100, B01000100 } },
+  {'F', { B01111100, B00010100, B00010100, B00000100, B00000100 } },
+  {'G', { B00111000, B01101100, B01000100, B01010100, B00110100 } },
+  {'H', { B01111100, B00010000, B00010000, B00010000, B01111100 } },
+  {'I', { B00000000, B01000100, B01111100, B01000100, B00000000 } },
+  {'J', { B01100000, B01000000, B01000000, B01000000, B01111100 } },
+  {'K', { B01111100, B00010000, B00111000, B00101000, B01000100 } },
+  {'L', { B01111100, B01000000, B01000000, B01000000, B01000000 } },
+  {'M', { B01111100, B00001000, B00010000, B00001000, B01111100 } },
+  {'N', { B01111100, B00000100, B00001000, B00010000, B01111100 } },
+  {'O', { B00111000, B01000100, B01000100, B01000100, B00111000 } },
+  {'P', { B01000100, B01111100, B01010100, B00010100, B00011000 } },
+  {'Q', { B00111000, B01000100, B01100100, B11000100, B10111000 } },
+  {'R', { B01111100, B00010100, B00010100, B00110100, B01011000 } },
+  {'S', { B01011000, B01010100, B01010100, B01010100, B00100100 } },
+  {'T', { B00000100, B00000100, B01111100, B00000100, B00000100 } },
+  {'U', { B01111100, B01000000, B01000000, B01000000, B01111100 } },
+  {'V', { B01111100, B00100000, B00010000, B00001000, B00000100 } },
+  {'W', { B01111100, B01100000, B01111100, B01000000, B01111100 } },
+  {'X', { B01000100, B00101000, B00010000, B00101000, B01000100 } },
+  {'Y', { B00000100, B00001000, B01110000, B00001000, B00000100 } },
+  {'Z', { B01000100, B01100100, B01010100, B01001100, B01100100 } },
+  {'.', { B01000000, B01000000, B00000000, B00000000, B00000000 } },
+  {'-', { B00010000, B00010000, B00010000, B00010000, B00010000 } },
+  {',', { B10000000, B10100000, B01100000, B00000000, B00000000 } },
+  {'/', { B01000000, B00100000, B00010000, B00001000, B00000100 } },
+  {'@', { B00111000, B01000100, B01010100, B01000100, B00111000 } },
+  {'$', { B01011000, B01010100, B11010110, B01010100, B00100100 } },
+  {'*', { B00000000, B00000000, B00000100, B00001110, B00000100 } }
+
+};
+
+#define N_HELL  (sizeof(helltab)/sizeof(helltab[0]))
+
+void helldelay()
+{
+  //Slow
+  delay(64);
+  delayMicroseconds(900);
+  
+  //Feld-Hell
+  //delay(8);
+  //delayMicroseconds(160);
+
+}
+
+void on()
+{
+  digitalWrite(txPin, HIGH);
+  helldelay();
+  digitalWrite(txPin, LOW);
+}
+
+void hellsend(char c)
+{
+  int i,j,q ;
+  if (c == ' ') {
+      for (int d=0; d<14; d++){
+        helldelay();  
+      }
+    return ;
+  }
+  for (i=0; i<N_HELL; i++) {
+    if (helltab[i].c == c) {
+      //Serial.print(helltab[i].c) ;
+      
+      for (j=0; j<=4; j++) 
+      {
+        byte mask = B10000000;
+        for (q=0; q<=6; q++)
+        {      
+          if(helltab[i].hellpat[j] & mask) {
+            on();
+          } else {
+            helldelay();
+          }
+          mask >>= 1;
+        }
+      }
+      for (int d=0; d<14; d++){
+        helldelay();  
+      }
+      return ;
+    }
+  }
+  /* if we drop off the end, then we send a space */
+}
+
+void hellsendmsg(char *str)
+{
+  while (*str)
+    hellsend(*str++) ;
+}
 
 // RTTY Functions - from RJHARRISON's AVR Code
 void rtty_txstring (char * string)
@@ -77,7 +189,9 @@ void setup()
   pinMode(ledPin, OUTPUT);
   
   Serial1.begin(9600);
-  Serial.begin(9600);
+  
+  analogReference(EXTERNAL); 
+  analogReadAveraging(32);
   
   digitalWrite(enPin, HIGH);
   
@@ -101,6 +215,10 @@ void loop()
     setupGPS();
   }
   
+  if((count % 100) == 0) {
+    setupGPS();
+  }
+  
   delay(100);
   gps_check_lock();
   delay(100);
@@ -108,14 +226,21 @@ void loop()
   delay(100);
   gps_get_position();
   
+  voltage = analogRead(8);
+  int_temp =  analogRead(38);
   
-  n=sprintf (superbuffer, "$$ATLAS,%d,%02d:%02d:%02d,%ld,%ld,%ld,%d,%d,%d,%d", count, gps_hour, gps_minute, gps_second, lat, lon, alt, sats, lock, navmode, GPSerror);
+  n=sprintf (superbuffer, "$$ATLAS,%d,%02d:%02d:%02d,%ld,%ld,%ld,%d,%d,%d,%d,%d,%d", count, gps_hour, gps_minute, gps_second, lat, lon, alt, sats, lock, navmode, GPSerror, voltage,int_temp);
   n = sprintf (superbuffer, "%s*%04X\n", superbuffer, gps_CRC16_checksum(superbuffer));
   
-  Serial.println(superbuffer);
+  //Serial.println(superbuffer);
   
-  rtty_txstring("$$");
-  rtty_txstring(superbuffer);
+  if((count % 20) == 0){
+    hellsendmsg(superbuffer);
+  }
+  else {
+    rtty_txstring("$$");
+    rtty_txstring(superbuffer);
+  }
   
 }
 
